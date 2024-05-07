416416389cbc8de4b2eb7887659500eb9dcc0e39
satish_parmar_web <satish.paramr.web@gmail.com>
2024-05-07 10:25:24 +0530

    Admin Layouts butyfiey with boostrap part

---
+++ resources/views/admin/layouts/partials/footer-scripts.blade.php
@@ -0,0 +1,7 @@
+<script src="{{asset('Admin/assets/static/js/components/dark.js')}}"></script>
+<script src="{{asset('Admin/assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+<script src="{{asset('Admin/assets/compiled/js/app.js')}}"></script>
+<!-- Need: Apexcharts -->
+<script src="{{asset('Admin/assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{asset('Admin/assets/static/js/pages/dashboard.js')}}"></script>
+
