d0a865281ac2e2dee3be966d8ff6c0d8c46197ef
satish_parmar_web <satish.paramr.web@gmail.com>
2024-06-15 12:59:10 +0530

    update js version

--- resources/views/layouts/partials/footer-scripts.blade.php
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -25,10 +25,10 @@
    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script>
    <script src="{{asset('plugins/google-map/gmap.js')}}"></script>
 
-   <script src="{{asset('js/script.js')}}"></script>
+   <script src="{{asset('js/script.js')}}?1"></script>
 
 
-   <script src="{{asset('js/common.js')}}"></script>
+   <script src="{{asset('js/common.js')}}?1"></script>
 
    @stack('scripts')
 
